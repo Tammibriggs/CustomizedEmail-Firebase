4eab828fb080b2526ce2224b295c82c98209b654
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-02-26 14:40:52 +0100

    made register verify and login route unaccessible by verified users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import Profile from './Profile'
 import Register from './Register'
 import VerifyEmail from './VerifyEmail';
@@ -14,24 +14,34 @@ function App() {
 
   const [currentUser, setCurrentUser] = useState(null)
   const [timeActive, setTimeActive] = useState(false)
+  const [fetching, setFetching] = useState(false)
 
   useEffect(() => {
+    setFetching(true)
     onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
+      setFetching(false)
     })
   }, [])
 
   return (
     <Router>
-      <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
-        <Switch>
-          <PrivateRoute exact path="/" component={Profile} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path='/verify-email' component={VerifyEmail} /> 
-        </Switch>  
-      </AuthProvider>
-  </Router>
+      {!fetching &&
+        <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
+          <Switch>
+            <PrivateRoute exact path="/" component={Profile} />
+            {!currentUser?.emailVerified
+              ? <>
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path='/verify-email' component={VerifyEmail} /> 
+                </>
+              : <Redirect to='/'/>
+            }
+          </Switch>  
+        </AuthProvider>
+      }
+    </Router>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBabo7XfrbgD_z4qIsf3KWHnPwwqUBM5xI",
-  authDomain: "fir-user-reg-auth.firebaseapp.com",
-  projectId: "fir-user-reg-auth",
-  storageBucket: "fir-user-reg-auth.appspot.com",
-  messagingSenderId: "720211998290",
-  appId: "1:720211998290:web:8291fab2697dbaafc2d18b"
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID"
 }
 
 // Initialize Firebase and Firebase Authentication
